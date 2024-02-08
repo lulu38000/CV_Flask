f13ca02d0de843848e621b09bbf276b714aca24e
lulu38000 <156298153+lulu38000@users.noreply.github.com>
2024-02-08 11:22:16 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,5 +4,5 @@ CREATE TABLE clients (
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     nom TEXT NOT NULL,
     prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+    message TEXT NOT NULL
 );
